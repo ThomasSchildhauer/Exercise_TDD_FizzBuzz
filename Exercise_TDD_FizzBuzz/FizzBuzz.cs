d0e171d811fab89885fc0b2d6745fe82ea8520a8
Thomas Schildhauer <Thomas.Schildhauer@outlook.com>
2019-07-13 20:00:15 +0200

    Project builds

---
+++ Exercise_TDD_FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_TDD_FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string DetermineFizzBuzz(int number)
+        {
+            return "0";
+        }
+    }
+}
--- Exercise_TDD_FizzBuzzTest/FizzBuzzTest.cs
+++ Exercise_TDD_FizzBuzzTest/FizzBuzzTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Exercise_TDD_FizzBuzz;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +12,17 @@ namespace Exercise_TDD_FizzBuzzTest
     [TestClass]
     public class FizzBuzzTest
     {
+        [TestMethod]
         public void DetermineFizzBuzz_Should_Return_0_For_0()
         {
             // ARRANGE
-
+            string expected = "0";
+            int input = 0;
+            FizzBuzz SUT = new FizzBuzz();
             // RUN
-
+            var actual = SUT.DetermineFizzBuzz(input);
             // ASSERT
+            expected.Should().Be(actual);
         }
     }
 }
